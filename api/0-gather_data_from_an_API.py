030b00da89f756983c06b2ecf3c501171e78376f
UN-Bonasse <b.ndekezi@alustudent.com>
2026-07-09 10:00:57 +0000

    Add task 0: gather data from API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that gathers data from an API for a given employee ID."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url, params={"userId": employee_id}).json()
+
+    employee_name = user.get("name")
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(done_tasks), total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
